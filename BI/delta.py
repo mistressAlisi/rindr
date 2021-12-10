565d226e26f6a1dd51c5b5093e58c00679dd8ab2
Cameron Laird <cameron.laird@Cameron.L-MBPro>
2021-12-09 18:50:19 -0600

    Many refinements, principal among which is modern type annotation.

--- BI/delta.py
+++ BI/delta.py
@@ -1,54 +1,100 @@
+#!/usr/bin/python3
+"""
+Run "mypy --disallow-untyped-defs --ignore-missing-imports \
+          --show-error-codes --strict-equality delta.py".
+
+The holidays module lacks type hints.
+
+Next need:  explicit Pytests.
+
+Notice the imports are punctuated in an isort style.
+"""
+from datetime import datetime, timedelta
 import sys
-import datetime
+
 import holidays
+
+
+EVENT = datetime
+fromisoformat = datetime.fromisoformat
+
 
 class TimeCalc():
     BD_START = 10
     BD_STOP = 17
     
-    def get_beginning_of_day(self,event):
-        return datetime.datetime.fromisoformat(f"{self.get_day_string(event)} {self.BD_START}")
+    def _get_beginning_of_day(self,event: EVENT) -> EVENT:
+        return fromisoformat(f"{self._get_day_string(event)} {self.BD_START}")
 
-    def get_end_of_day(self,event):
-        return datetime.datetime.fromisoformat(f"{self.get_day_string(event)} {self.BD_STOP}")
+    def _get_end_of_day(self,event: EVENT) -> EVENT:
+        return fromisoformat(f"{self._get_day_string(event)} {self.BD_STOP}")
 
-    def get_day(self,event):
-        return datetime.datetime.fromisoformat(self.get_day_string(event))
+    def _get_day(self,event: EVENT) -> EVENT:
+        return fromisoformat(self._get_day_string(event))
 
-    def get_day_string(self,event):
+    def _get_day_string(self,event: EVENT) -> str:
         return f"{event.year}-{event.month}-{event.day}"
 
-    def get_next_day(self,event):
-        return self.get_day(event) + datetime.timedelta(days=1)
+    def _get_next_day(self,event: EVENT) -> EVENT:
+        return self._get_day(event) + timedelta(days=1)
 
-    def is_work_day(self,event):
+    def _is_work_day(self,event: EVENT) -> bool:
         if event.date() in holidays.US():
             return False
         if event.weekday() >= 5:
             return False
         return True
 
-
-    def business_lapse(self,request, response):
-        """ Note there is NO allowance for weekends or holidays.  Those can appear
-            as a later upgrade.
+    def business_lapse(self,request: EVENT, response: EVENT) -> timedelta:
+        """ This is the entry point most clients will want to use.
         """
         if request > response:
             raise RuntimeError(f"How can there have been a response at {response} to a *later* request at {request}?")
         if request != response:
-            beginning_of_request_day = self.get_beginning_of_day(request)
+            beginning_of_request_day = self._get_beginning_of_day(request)
             if request < beginning_of_request_day:
                 return self.business_lapse(min(beginning_of_request_day, response), response)
-            end_of_response_day = self.get_end_of_day(response)
+            end_of_response_day = self._get_end_of_day(response)
             if end_of_response_day < response:
                 return self.business_lapse(request, max(end_of_response_day, request))
-            request_day = self.get_day(request)
-            new_request = self.get_next_day(request)
-            if not self.is_work_day(request_day):
+            request_day = self._get_day(request)
+            new_request = self._get_next_day(request)
+            if not self._is_work_day(request_day):
                 return self.business_lapse(new_request, max(response, new_request))
-            if not self.is_work_day(request_day) or request_day != self.get_day(response):
-                assert request_day < self.get_day(response)
-                return (max(request, self.get_end_of_day(request)) - request) + \
+            assert self._is_work_day(request_day)
+            if request_day != self._get_day(response):
+                assert request_day < self._get_day(response)
+                return (max(request, self._get_end_of_day(request)) - request) + \
                         self.business_lapse(new_request, max(response, new_request))
             assert beginning_of_request_day <= request <= response <= end_of_response_day
         return response - request
+
+
+def main() -> None:
+    """ Examples:
+            ./delta.py "2021-10-27 03:45" "2021-10-29 11:08"
+            ./delta.py "2021-10-27 03:45" "2021-10-29 11:08"
+            ./delta.py "2021-10-27 03:45" "2021-10-29 20:08"
+            ./delta.py "2021-10-27 13:45" "2021-10-27 14:08"
+            ./delta.py "2021-10-27 13:45" "2021-10-28 14:08"
+            ./delta.py "2021-10-27 03:45" "2021-10-27 05:58"
+            ./delta.py "2021-10-27 10:00" "2021-10-28 17:00"
+            ./delta.py "2021-10-27 10:00" "2021-10-30 10:20"
+            ./delta.py "2021-10-27 18:00" "2021-10-27 20:20"
+            ./delta.py "2021-11-27 03:45" "2021-11-29 20:08"
+            ./delta.py "2021-11-27 13:45" "2021-11-27 14:08"
+            ./delta.py "2021-11-27 13:45" "2021-11-28 14:08"
+            ./delta.py "2021-11-27 03:45" "2021-11-27 05:58"
+            ./delta.py "2021-11-27 10:00" "2021-11-28 17:00"
+            ./delta.py "2021-11-27 10:00" "2021-11-30 10:20"
+            ./delta.py "2021-11-27 18:00" "2021-11-27 20:20"
+    """
+    request_time = fromisoformat(sys.argv[1])
+    response_time = fromisoformat(sys.argv[2])
+    print(f"Times are {request_time} and {response_time}.")
+    print(f"Difference is {response_time - request_time}.")
+    print(f"Business lapse is {TimeCalc().business_lapse(request_time, response_time)}.")
+
+
+if __name__ == "__main__":
+    main()
